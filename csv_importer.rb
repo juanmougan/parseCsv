fef0e776c20004806846e854188b30e4e8639284
Juan Manuel Mougan <juanmougan@gmail.com>
2015-08-17 18:16:36 -0300

    CSV file name is now a parameter

--- csv_importer.rb
+++ csv_importer.rb
@@ -6,7 +6,13 @@ require_relative 'model/subject'
 
 require_relative 'csv_parser'
 
-# Parse the CSV
-csv_parser = CsvParser.new('input.csv')		# TODO this should be a command line parameter
-raw_student_rows = csv_parser.parse_file
+def check_arguments
+	if ARGV.length < 1
+		raise RuntimeError, "Missing CSV input file"
+	end
+	return ARGV[0]
+end
 
+# Parse the CSV
+csv_parser = CsvParser.new(check_arguments)
+raw_student_rows = csv_parser.parse_file
