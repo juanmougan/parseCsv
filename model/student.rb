f8daa6a6399efc60266603e053c85f1804ace371
Juan Manuel Mougan <juanmougan@gmail.com>
2015-08-17 19:15:26 -0300

    Bad copy-paste fixed

--- model/student.rb
+++ model/student.rb
@@ -1,9 +1,12 @@
-class Subject
-  attr_accessor :id, :name, :code, :career
+class Student
+  attr_accessor :id, :first_name, :last_name, :file_number, :career, :subjects
 
-  def initialize(name, code, career)
-    @name = name
-    @code = code
-    @career = career
+  def initialize(id, first_name, last_name, file_number, career, subjects)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @file_number = file_number
+    @career = career 
+    @subjects = subjects
   end
 end
