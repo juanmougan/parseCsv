0d966c72552f0d43116573b071944577a396f50a
Juan Manuel Mougan <juanmougan@gmail.com>
2015-08-17 18:48:38 -0300

    Will parse the student (database) ID to an Integer

--- csv_parser.rb
+++ csv_parser.rb
@@ -11,7 +11,7 @@ class CsvParser
 		rawStudentRows = []
 		CSV.foreach(File.path(@file_path), {:headers => true}) do |col|
     		# Where col corresponds to a zero-based value/column in the csv
-    		rawStudentRows << StudentRow.new(col[0], col[1], col[2], col[3], col[4], col[5], col[6])
+    		rawStudentRows << StudentRow.new(Integer(col[0]), col[1], col[2], col[3], col[4], col[5], col[6])
 		end
 		return rawStudentRows
 	end
